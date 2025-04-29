6324a444fe1de04c0506797849edc580a8826540
carleighsmith544 <w10111544@usm.edu>
2025-04-28 21:52:44 -0500

    Update UserController.cs

--- Project/Controllers/UserController.cs
+++ Project/Controllers/UserController.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SavorySweets.Project.Models;
 using SavorySweets.Project.Data;
-using Microsoft.Maui.Storage;
+
 
 namespace SavorySweets.Project.Controllers
 {
     public class UserController
     {
-        private readonly UserDataService _userDataService;
+        private readonly UserDataService _userDataService; //service to interact with user data
 
         public UserController()
         {
             _userDataService = new UserDataService();
         }
 
+        //registers a new user
         public bool Register(User newUser)
         {
             return _userDataService.Register(newUser);
         }
 
+        //attempts to log a user in with email and password
         public bool Login(string email, string password)
         {
             var user = _userDataService.Login(email, password);
             if (user != null)
             {
+                //save the logged-in user's ID 
                 Preferences.Set("LoggedInUserId", user.ID);
                 return true;
             }
             return false;
         }
 
+        //logs out the current user
         public void Logout()
         {
             Preferences.Remove("LoggedInUserId");
         }
 
+        //retrieves the currently logged-in user's information
         public User? GetLoggedInUser()
         {
             int id = Preferences.Get("LoggedInUserId", -1);
@@ -46,7 +46,7 @@ namespace SavorySweets.Project.Controllers
             return users.FirstOrDefault(u => u.ID == id);
         }
 
-
+        //updates an existing user's information
         public void UpdateUserInfo(User updatedUser)
         {
             _userDataService.UpdateUser(updatedUser);
