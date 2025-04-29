884c348784a70fc4ba7a39d114b5d9d82a59c78f
carleighsmith544 <w10111544@usm.edu>
2025-04-28 22:21:26 -0500

    Update GlutenFreePage.xaml.cs

--- Project/Views/GlutenFreePage.xaml.cs
+++ Project/Views/GlutenFreePage.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SavorySweets.Project.Controllers;
+
 
 namespace SavorySweets.Project.Views
 {
@@ -18,6 +13,8 @@ namespace SavorySweets.Project.Views
             _recipeController = new RecipeController();
             LoadGlutenFreeRecipes();
         }
+
+        // Loads recipes that are categorized as "Gluten-Free"
         private void LoadGlutenFreeRecipes()
         {
             var glutenFree = _recipeController.Recipes
@@ -28,6 +25,7 @@ namespace SavorySweets.Project.Views
             glutenFreeCollection.ItemsSource = new ObservableCollection<RecipeDisplayView>(glutenFree);
         }
 
+        //handles when the favorite icon is clicked to toggle favorite status
         private void OnToggleFavoriteClicked(object sender, EventArgs e)
         {
             if (sender is ImageButton button && button.CommandParameter is int id)
@@ -37,6 +35,7 @@ namespace SavorySweets.Project.Views
             }
         }
 
+        //handles when a gluten-free recipe is selected from the CollectionView
         private void OnRecipeSelected(object sender, SelectionChangedEventArgs e)
         {
             if (e.CurrentSelection.FirstOrDefault() is RecipeDisplayView selected)
