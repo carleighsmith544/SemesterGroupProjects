e0b26503b34f1a50b3bf3922b824c28d6b51713d
carleighsmith544 <w10111544@usm.edu>
2025-04-28 21:54:32 -0500

    Update FavoriteDataService.cs

--- Project/Data/FavoriteDataService.cs
+++ Project/Data/FavoriteDataService.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 using SavorySweets.Project.Models;
 
 namespace SavorySweets.Project.Data
 {
     public class FavoriteDataService
     {
+        //file path to store the favorites JSON file
         private readonly string _filePath = Path.Combine(FileSystem.AppDataDirectory, "favorites.json");
 
+        //loads the list of favorites from the JSON file
         public List<Favorite> LoadFavorites()
         {
+            //checks if the file exists
             if (File.Exists(_filePath))
             {
                 string json = File.ReadAllText(_filePath);
                 return JsonSerializer.Deserialize<List<Favorite>>(json) ?? new List<Favorite>();
             }
+            //if file doesn't exist, return empty list
             return new List<Favorite>();
         }
 
+        //saves the list of favorites to the JSON file
         public void SaveFavorites(List<Favorite> favorites)
         {
             string json = JsonSerializer.Serialize(favorites);
