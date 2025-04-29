ce142b5f94dd768f5b2b1f31cf0fd4aa3171e9b7
carleighsmith544 <w10111544@usm.edu>
2025-04-28 22:30:56 -0500

    Update PopularRecipesPage.xaml.cs

--- Project/Views/PopularRecipesPage.xaml.cs
+++ Project/Views/PopularRecipesPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -10,7 +10,7 @@ namespace SavorySweets.Project.Views
 {
     public partial class PopularRecipesPage : ContentPage
     {
-        private readonly RecipeController _recipeController;
+        private readonly RecipeController _recipeController; 
 
         public PopularRecipesPage()
         {
@@ -18,29 +18,35 @@ namespace SavorySweets.Project.Views
             _recipeController = new RecipeController();
             LoadPopularRecipes();
         }
+
+        // Loads recipes that are categorized as "Popular"
         private void LoadPopularRecipes()
         {
             var popular = _recipeController.Recipes
-                .Where(r => r.Category != null && r.Category.ToLower().Contains("popular"))
-                .Select(r => new RecipeDisplayView(r))
+                .Where(r => r.Category != null && r.Category.ToLower().Contains("popular")) 
+                .Select(r => new RecipeDisplayView(r)) 
                 .ToList();
 
+            // Bind the popular recipes to the CollectionView
             popularRecipesCollection.ItemsSource = new ObservableCollection<RecipeDisplayView>(popular);
         }
 
+        // Handles when the favorite icon is clicked to toggle favorite status
         private void OnToggleFavoriteClicked(object sender, EventArgs e)
         {
             if (sender is ImageButton button && button.CommandParameter is int id)
             {
                 _recipeController.ToggleFavorite(id);
-                LoadPopularRecipes(); // Refresh
+                LoadPopularRecipes(); 
             }
         }
 
+        // Handles when a popular recipe is selected from the CollectionView
         private void OnRecipeSelected(object sender, SelectionChangedEventArgs e)
         {
             if (e.CurrentSelection.FirstOrDefault() is RecipeDisplayView selected)
             {
+                
                 var recipe = _recipeController.Recipes.FirstOrDefault(r => r.Id == selected.Id);
                 if (recipe != null)
                 {
