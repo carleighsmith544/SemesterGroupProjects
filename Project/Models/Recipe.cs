48ff0448e4f96390726da99516d45ce762570689
carleighsmith544 <w10111544@usm.edu>
2025-04-28 22:02:17 -0500

    Update Recipe.cs

--- Project/Models/Recipe.cs
+++ Project/Models/Recipe.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace SavorySweets.Project.Models
 {
     public class Recipe
     {
-        public int Id { get; set; }
+        public int Id { get; set; } //unique id for recipe
         public string Title { get; set; } = "";
         public string Description { get; set; } = "";
         public string Ingredients { get; set; } = "";
