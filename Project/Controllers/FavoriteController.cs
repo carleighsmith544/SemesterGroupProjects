a0e46464d967b913fd25ec1aa65bbe0c3cb79207
carleighsmith544 <w10111544@usm.edu>
2025-04-28 21:40:51 -0500

    Update FavoriteController.cs

--- Project/Controllers/FavoriteController.cs
+++ Project/Controllers/FavoriteController.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Linq;
 using SavorySweets.Project.Models;
 
 
 namespace SavorySweets.Project.Controllers
 {
-
+    //controller class
     public class FavoriteController
     {
+        //static list to store all favorite entries
         private static List<Favorite> _favorites = new();
+        
+        //adds a recipe to a user's list of favorites if it is not already favorited
+
         public void AddFavorite(int userId, int recipeId)
         {
+            //if the favorite already exists, avoid a duplicatiion
             if (!_favorites.Any(f => f.UserId == userId && f.RecipeId == recipeId))
             {
+                //add new favorite
                 _favorites.Add(new Favorite
                 {
                     Id = _favorites.Count + 1,
@@ -27,20 +26,24 @@ namespace SavorySweets.Project.Controllers
             }
         }
 
+        //function to remove favorite from list
         public void RemoveFavorite(int userId, int recipeId)
         {
             var favorite = _favorites.FirstOrDefault(f => f.UserId == userId && f.RecipeId == recipeId);
+            //if favorite exists, remove it
             if (favorite != null)
             {
                 _favorites.Remove(favorite);
             }
         }
 
+        //function to check if a specific recipe is favorited by a specific user
         public bool IsFavorited(int userId, int recipeId)
         {
             return _favorites.Any(f => f.UserId == userId && f.RecipeId == recipeId);
         }
 
+        //function to retrieve all the favorite recipes 
         public List<Favorite> GetFavoritesForUser(int userId)
         {
             return _favorites.Where(f => f.UserId == userId).ToList();
